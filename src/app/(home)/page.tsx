290c50627d984069230c833423675dd0bdba9bff
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-08 20:07:10 +0530

    add suspense

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,79 +1,9 @@
+import { Suspense } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import ProductCard from "./_components/product-card";
-import { Category, Product } from "@/lib/types";
-
-const products = [
-  {
-    _id: "1",
-    name: "Product 1",
-    description: "Product 1",
-    image: "/pizza-main.png",
-    price: 500,
-  },
-  {
-    _id: "2",
-    name: "Product 1",
-    description: "Product 1",
-    image: "/pizza-main.png",
-    price: 400,
-  },
-  {
-    _id: "3",
-    name: "Product 1",
-    description: "Product 1",
-    image: "/pizza-main.png",
-    price: 300,
-  },
-  {
-    _id: "4",
-    name: "Product 1",
-    description: "Product 1",
-    image: "/pizza-main.png",
-    price: 200,
-  },
-  {
-    _id: "5",
-    name: "Product 1",
-    description: "Product 1",
-    image: "/pizza-main.png",
-    price: 100,
-  },
-];
+import ProductList from "./_components/product-list";
 
 export default async function Home() {
-  const categoryResponse = await fetch(
-    `${process.env.BACKEND_URL}/api/catalog/categories?perPage=100`,
-    {
-      next: {
-        revalidate: 3600, // 1 hour
-      },
-    }
-  );
-
-  if (!categoryResponse.ok) {
-    throw new Error("Failed to fetch tenants information");
-  }
-
-  const categories: Category[] = await categoryResponse.json();
-
-  // todo: add pagination and dynamic tenantId;
-  const productResponse = await fetch(
-    `${process.env.BACKEND_URL}/api/catalog/products?perPage=100&tenantId=10`,
-    {
-      next: {
-        revalidate: 3600, // 1 hour
-      },
-    }
-  );
-
-  if (!productResponse.ok) {
-    throw new Error("Failed to fetch products information");
-  }
-
-  const products: { data: Product[] } = await productResponse.json();
-
   return (
     <>
       {/* Hero Section */}
@@ -103,40 +33,14 @@ export default async function Home() {
       </section>
 
       {/* Tab Section */}
-      <section>
-        <div className="container py-12">
-          <Tabs defaultValue={categories[0]._id}>
-            <TabsList>
-              {categories?.map((category) => {
-                return (
-                  <TabsTrigger
-                    key={category?._id}
-                    value={category._id}
-                    className="text-base"
-                  >
-                    {category.name}
-                  </TabsTrigger>
-                );
-              })}
-            </TabsList>
-            {categories?.map((category) => {
-              return (
-                <TabsContent value={category._id} key={category._id}>
-                  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-6">
-                    {products?.data
-                      .filter((product) => product.categoryId === category._id)
-                      .map((product) => {
-                        return (
-                          <ProductCard key={product._id} product={product} />
-                        );
-                      })}
-                  </div>
-                </TabsContent>
-              );
-            })}
-          </Tabs>
-        </div>
-      </section>
+      <Suspense
+        // add skeleton loader
+        fallback={
+          <div className="w-20 h-20 border-t-2 border-l-2 border-primary animate-spin rounded-full my-4 mx-auto" />
+        }
+      >
+        <ProductList />
+      </Suspense>
     </>
   );
 }
