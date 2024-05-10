c016fa22154c14b7d02215ac77a3a2310d009826
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-10 12:36:25 +0530

    set value and defaultValue select on refresh

--- src/components/custom/tenant-select.tsx
+++ src/components/custom/tenant-select.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Tenant } from "@/lib/types";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   Select,
   SelectContent,
@@ -8,24 +9,38 @@ import {
   SelectTrigger,
   SelectValue,
 } from "../ui/select";
-import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const TenantSelect = ({ resutrants }: { resutrants: { data: Tenant[] } }) => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const resutrantId = searchParams.get("resutrantId");
 
   const handleValueChange = (resutrantId: string) => {
     router.push(`/?resutrantId=${resutrantId}`);
   };
 
+  const defaultResutrantId = resutrants.data[0].id;
+
+  useEffect(() => {
+    if (resutrants.data[0].id) {
+      router.push(`/?resutrantId=${defaultResutrantId}`);
+    }
+  }, [defaultResutrantId, resutrants.data, router]);
+
   return (
-    <Select onValueChange={handleValueChange}>
+    <Select
+      value={resutrantId || ""}
+      defaultValue={defaultResutrantId || ""}
+      onValueChange={handleValueChange}
+    >
       <SelectTrigger className="w-[180px] focus:ring-0">
         <SelectValue placeholder="Select Restaurant" />
       </SelectTrigger>
       <SelectContent>
         {resutrants.data.map((resutrant) => {
           return (
-            <SelectItem value={resutrant.id} key={resutrant.id}>
+            <SelectItem value={String(resutrant.id)} key={resutrant.id}>
               {resutrant.name}
             </SelectItem>
           );
