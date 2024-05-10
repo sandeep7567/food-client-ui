c9f87a25b40726371c7479ad3ce774ba236eb897
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-10 18:10:06 +0530

    add a cart page

---
+++ src/app/(cart)/cart/page.tsx
@@ -0,0 +1,17 @@
+import CartItems from "./CartItems/CartItems";
+
+const Cart = () => {
+  return (
+    <section>
+      <div className="container mx-auto py-6">
+        <h1 className="text-lg font-bold">Shopping Cart</h1>
+
+        <div className="bg-white rounded-lg p-6 mt-6">
+          <CartItems />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cart;
